fabde50d6eece8fcc43f83c7daa94c280cea07dd
Eduardo j Ferreira <eddie.friends.inbox@gmail.com>
2019-05-19 23:53:35 -0300

    pagina de contato e php teste

---
+++ email.php
@@ -0,0 +1,26 @@
+<?php
+
+if(isset($_POST['email']) && !empty($_POST['email'])) {
+
+$nome = addslashes($_POST['name']);
+$email = addslashes($_POST['email']);
+$mensagem = addslashes($_POST['message']);
+
+$to = "eddie.friends.inbox@gmail.com";
+
+$subject = "Contato - Página Github";
+
+$body = "Nome: ".$nome."\n"."Email: ".$email."\n"."Mensagem: ".$mensagem;
+
+$header = "From: eddy.fryends@gmail.com"."\r\n"."Reply-To:".$email."\r\n"."x-mailer:PHP/".hpversion();
+
+if(mail($to,$subject,$body,$header)){
+	echo("E-mail enviado com sucesso!");
+}else{
+	echo("Erro ao enviar e-mail!");
+}
+
+
+
+}
+?>	
